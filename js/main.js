8d85996fe7eddb9913ef9db2112b28ebe00896fd
AddictionSe <94913747+AddictionSe@users.noreply.github.com>
2023-12-17 17:54:13 +0800

    name

--- js/main.js
+++ js/main.js
@@ -408,7 +408,7 @@ window.addEventListener("load", function () {
       //移动端隐藏更多页面
       $("#container").attr("class", "container");
       $("#change").html("Hello&nbsp;World&nbsp;!");
-      $("#change1").html("一个不正经的Up");
+      $("#change1").html("最爱你的WYHUp");
 
       //移动端隐藏弹窗页面
       $("#box").css("display", "none");
