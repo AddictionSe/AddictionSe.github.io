53b04c7f4a950ee979552facc1d44330b49044d7
AddictionSe <94913747+AddictionSe@users.noreply.github.com>
2023-12-19 03:08:34 +0800

    surprise

---
+++ js/photo.js
@@ -0,0 +1,167 @@
+const photosInfo = [
+    {
+        cephalogram_url: './img/Sagaya/1.jpg',
+        name: '一起看的电影',
+        introduction: '还记得有多少场嘛 ～',
+        size: 'width-24',
+        children: [
+            {
+                children_url: './img/Sagaya/2.jpg',
+                children_introduction: `第一次看电影在SFC上影影城，那天我还给你拍了照片哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈还蛮可爱的～～`,
+            },
+            {
+                children_url: './img/Sagaya/3.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: './img/Sagaya/4.jpg',
+                children_introduction: `这是第三次去看了，还在考研期间，短短三周就看了三次，不愧是咱俩！`,
+            },
+            {
+                children_url: './img/Sagaya/5.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: './img/Sagaya/6.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: './img/Sagaya/7.jpg',
+                children_introduction: `真的看了好多好多电影，还有好多来我手机上看❤️`,
+            },
+        ]
+    },
+    {
+        cephalogram_url: './img/An_Uncommon_Place_III/1.jpg',
+        name: '第一次去旅游！！',
+        introduction: '大连见！！',
+        size: 'width-11',
+        children: [
+            {
+                children_url: './img/An_Uncommon_Place_III/5.jpg',
+                children_introduction: '我老婆的拍照技术真好，这张现在还是我微信背景',
+            },
+            {
+                children_url: './img/An_Uncommon_Place_III/3.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: './img/An_Uncommon_Place_III/4.jpg',
+                children_introduction: `咱们住的君悦真的很不错风景很好，服务态度也不错 ～`,
+            },
+            {
+                children_url: './img/An_Uncommon_Place_III/2.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: './img/An_Uncommon_Place_III/6.jpg',
+                children_introduction: `希尔顿的城堡看着好美呀，以后咱们结婚可以在这种地方！！`,
+            },
+        ]
+    },
+    {
+        cephalogram_url: './img/Between_Sea_ Sky/1.jpg',
+        name: '成都之旅',
+        introduction: '我愿称之为住过的最舒服的酒店',
+        size: 'width-11',
+        children: [
+            {
+                children_url: './img/Between_Sea_ Sky/2.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: './img/Between_Sea_ Sky/3.jpg',
+                children_introduction: `也算是看过熊猫了吧，赛博熊猫！！！`,
+            },
+            {
+                children_url: './img/Between_Sea_ Sky/4.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: './img/Between_Sea_ Sky/5.jpg',
+                children_introduction: `成都的酒店真的很不错，别的不知道，毕竟咱俩不怎么出酒店，嘿嘿`,
+            },
+        ]
+    },
+    {
+        cephalogram_url: './img/eye/1.jpg',
+        name: '其实从上次开始咱俩的旅行',
+        introduction: '已经变成了一种黏在一起的方式',
+        size: 'width-24',
+        children: [
+            {
+                children_url: './img/eye/2.jpg',
+                children_introduction: `咱们住的酒店，尊度好热，不过有你在我身边再热都无所谓`,
+            },
+            {
+                children_url: './img/eye/3.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: './img/eye/4.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: './img/eye/5.jpg',
+                children_introduction: '真的好爱好爱你！！',
+            },
+        ]
+    },
+    {
+        cephalogram_url: './img/F1_Illustrations/1.jpg',
+        name: '一些吃的',
+        introduction: '给这里的图片盘版真的好饿，还是大晚上的',
+        size: 'width-11',
+        children: [
+            {
+                children_url: './img/F1_Illustrations/2.jpg',
+                children_introduction: '吃了好多好多好多火锅哈哈哈哈哈',
+            },
+            {
+                children_url: './img/F1_Illustrations/3.jpg',
+                children_introduction: ``,
+            },
+            {
+                children_url: './img/F1_Illustrations/4.jpg',
+                children_introduction: '海底捞我好喜欢，咱俩都吃成金海会员了',
+            },
+            {
+                children_url: './img/F1_Illustrations/5.jpeg',
+                children_introduction: ``,
+            },
+            {
+                children_url: './img/F1_Illustrations/6.jpg',
+                children_introduction: '希望宝贝能天天像过生日那天一样开心，你健健康康开开心心最重要！',
+            },
+        ]
+    },
+    {
+        cephalogram_url: './img/Hong_Kong_Cinematic/1.jpg',
+        name: '一些我拍的照片',
+        introduction: '不知道你能记起来是什么时候嘛，笨蛋老婆',
+        size: 'width-11',
+        children: [
+            {
+                children_url: './img/Hong_Kong_Cinematic/2.jpg',
+                children_introduction: `你肯定想不起来，嘿嘿`,
+            },
+            {
+                children_url: './img/Hong_Kong_Cinematic/3.jpg',
+                children_introduction: `第一个情侣杯哦，我真的心里暗暗开心了很久`,
+            },
+            {
+                children_url: './img/Hong_Kong_Cinematic/4.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: './img/Hong_Kong_Cinematic/5.jpg',
+                children_introduction: '我才知道十月革命原来不发生在十月',
+            },
+            {
+                children_url: './img/Hong_Kong_Cinematic/6.jpg',
+                children_introduction: '咱们要好好的在一起哦宝贝,全世界最最最最最最最最最最最最爱你的王宇豪送给一周年的咱们俩。',
+            },
+        ]
+    },
+
+]
